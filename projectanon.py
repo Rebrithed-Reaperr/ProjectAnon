7735d67ac3f2803f4da3dd0d602ae6fb1e4b1098
Rebrithed-Reaperr <85047722+Rebrithed-Reaperr@users.noreply.github.com>
2021-06-03 13:41:57 -0500

    Update projectanon.py

--- projectanon.py
+++ projectanon.py
@@ -6,3 +6,25 @@ from colorama import Fore
 from time import sleep
 
 S = str(input(Fore.RED+"Press enter to start:"))
+if 'enter' or '99' in S:
+  time.sleep(0.50)
+  os.system(' clear ')
+   
+print(Fore.RED+" ██▓███   ██▀███   ▒█████   ▄▄▄██▀▀▀▓█████  ▄████▄  ▄▄▄█████▓    ▄▄▄       ███▄    █  ▒█████   ███▄    █ 
+time.sleep(0)
+print(Fore.RED+"▓██░  ██▒▓██ ▒ ██▒▒██▒  ██▒   ▒██   ▓█   ▀ ▒██▀ ▀█  ▓  ██▒ ▓▒   ▒████▄     ██ ▀█   █ ▒██▒  ██▒ ██ ▀█   █ 
+time.sleep(0)
+print(Fore.RED+"▓██░ ██▓▒▓██ ░▄█ ▒▒██░  ██▒   ░██   ▒███   ▒▓█    ▄ ▒ ▓██░ ▒░   ▒██  ▀█▄  ▓██  ▀█ ██▒▒██░  ██▒▓██  ▀█ ██▒
+time.sleep(0)
+print(Fore.RED+"▒██▄█▓▒ ▒▒██▀▀█▄  ▒██   ██░▓██▄██▓  ▒▓█  ▄ ▒▓▓▄ ▄██▒░ ▓██▓ ░    ░██▄▄▄▄██ ▓██▒  ▐▌██▒▒██   ██░▓██▒  ▐▌██▒
+time.sleep(0)
+print(Fore.RED+"▒██▒ ░  ░░██▓ ▒██▒░ ████▓▒░ ▓███▒   ░▒████▒▒ ▓███▀ ░  ▒██▒ ░     ▓█   ▓██▒▒██░   ▓██░░ ████▓▒░▒██░   ▓██░
+time.sleep(0)
+print(Fore.RED+"▒▓▒░ ░  ░░ ▒▓ ░▒▓░░ ▒░▒░▒░  ▒▓▒▒░   ░░ ▒░ ░░ ░▒ ▒  ░  ▒ ░░       ▒▒   ▓▒█░░ ▒░   ▒ ▒ ░ ▒░▒░▒░ ░ ▒░   ▒ ▒ 
+time.sleep(0)
+print(Fore.RED+"░▒ ░       ░▒ ░ ▒░  ░ ▒ ▒░  ▒ ░▒░    ░ ░  ░  ░  ▒       ░         ▒   ▒▒ ░░ ░░   ░ ▒░  ░ ▒ ▒░ ░ ░░   ░ ▒░
+time.sleep(0)
+print(Fore.RED+"░░         ░░   ░ ░ ░ ░ ▒   ░ ░ ░      ░   ░          ░           ░   ▒      ░   ░ ░ ░ ░ ░ ▒     ░   ░ ░ 
+time.sleep(0)
+print(Fore.RED+"            ░         ░ ░   ░   ░      ░  ░░ ░                        ░  ░         ░     ░ ░           ░ 
+time.sleep(0)
