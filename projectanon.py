075f104167a6ca934b20f36167a4960db8227d15
Rebrithed-Reaperr <85047722+Rebrithed-Reaperr@users.noreply.github.com>
2021-06-04 07:06:08 -0500

    Update projectanon.py

--- projectanon.py
+++ projectanon.py
@@ -5,7 +5,7 @@ from os import system, name
 from colorama import Fore
 from time import sleep
 
-S = str(input(Fore.RED+"Press enter to start:"))
+X = str(input(Fore.RED+"Press enter to start:"))
 if 'enter' or '99' in S:
   time.sleep(0.50)
   os.system(' clear ')
